699aca12f7f4625fc704758de0cc9f1c3248cadd
Matheus F. Dias <matheus.foscarinid@gmail.com>
2023-09-07 19:21:53 -0300

    feat(CRUD): adding create method and showing on list

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,33 @@
+import { TodoItem } from '@/components/TodoItem';
+import { prisma } from '@/db';
 import Link from 'next/link';
 
-export default function Home() {
-  return <>
-    <header className='flex justify-between items-center md-4'>
+const getTodos = async () => {
+  return prisma.todo.findMany();
+}
+
+const toggleTodo = async (id: string, complete: boolean) => {
+  "use server"
+
+  await prisma.todo.update({
+    where: { id },
+    data: { complete }
+  })
+}
+
+const removeTodo = async (id: string) => {
+  "use server"
+
+  await prisma.todo.delete({
+    where: { id }
+  })
+}
+
+export default async function Home() {
+  const todos = await getTodos();
+
+  return (<>
+    <header className='flex justify-between items-center mb-4'>
       <h1 className='text-2x1'>TODOs</h1>
       <Link
         className='border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none' 
@@ -11,5 +36,11 @@ export default function Home() {
         New
       </Link>
     </header>
-  </>
+
+    <ul className='pl-4'>
+      {todos.map(todo => (
+        <TodoItem key={todo.id} {...todo} toggleTodo={toggleTodo} />
+      ))}
+    </ul>
+  </>)
 }
